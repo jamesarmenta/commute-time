9ff7dc95e462ccb22a494421434c02cc50757342
James Armenta <James_Armenta@intuit.com>
2018-09-20 18:17:08 -0700

    Updated git script

--- push_log_and_averages.sh
+++ push_log_and_averages.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 DATE=`date '+%Y%m%d'`
-git add log.csv
+git add log_evening.csv
+git add log_morning.csv
 git commit -m "Commute data for ${DATE}"
 git push
--- updater.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-DATE=`date '+%Y%m%d'`
-COMMIT_MESSAGE="Logs for ${DATE}"
-
-git add log.csv && git commit -m "${COMMIT_MESSAGE}" && git push
