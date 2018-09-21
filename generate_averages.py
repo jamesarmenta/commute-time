9b4a849e4f79ffd95bd7649a656922b959cc66c5
James Armenta <James_Armenta@intuit.com>
2018-09-20 17:50:35 -0700

    Generate averages

--- generate_averages.py
+++ generate_averages.py
@@ -1,22 +1,47 @@
+import csv
+from collections import defaultdict
+
+averages = defaultdict(lambda: defaultdict(lambda: 0))
+
 try:
-    f = open('~/Documents/commute-time/log.csv','r')
-
-    averages = {
-     'Mon':{},
-     'Tue':{},
-     'Wed':{},
-     'Thu':{},
-     'Fri':{},
-     }
-
-    for line in f:
-        date,day,time,duration,route,text = line.splt(',');
-        time = int(round(float(time*10) / float(100))*10) # Round time to closest whole
-        print("time %s", time)
-        averages[day][time][value] = (averages[day][time][value]*averages[day][time][count]) + duration
-        averages[day][time][count]++;
-
-
+    with open("./log.csv", "r") as f:
+        reader = csv.reader(f, delimiter=",")
+        for i, line in enumerate(reader):
+            date, day, time, duration, route, text = line;
+            time = int(round(float(int(time)*10) / float(100))*10) # Round time to closest whole
+            # If it's 1760, it's actually 1800
+            if (time + 40) % 100 == 0:
+                time = time + 40
+            # skip mornings for now
+            if time < 1200:
+                continue
+            daytime = day + str(time);
+            averages[daytime]['value'] = averages[daytime]['value'] + int(duration)
+            averages[daytime]['count'] = averages[daytime]['count'] + 1
+            averages[daytime]['route'+route] = averages[daytime]['route'+route] + 1
     f.close()
+
+    final = []
+
+    for daytime in averages:
+        dt = averages[daytime]
+        average = int(round(dt['value'] / dt['count']))
+        routeCount = 0
+        route = ''
+        for key in dt:
+            if 'route' in key:
+                if dt[key] > routeCount:
+                    routeCount = dt[key]
+                    route = key
+        final.append({
+            'daytime': daytime,
+            'route': route,
+            'average': average
+        })
+
+    final = sorted(final, key=lambda k: k['daytime'])
+    for i in final:
+        print i['daytime'] + ',' + i['route'] + ',' + str(i['average'])
+
 except Exception as e:
     print e
