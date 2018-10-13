ee2c8b9480aeba30e12f74f3066bc6e7bad67f93
James Armenta <James_Armenta@intuit.com>
2018-10-12 17:30:29 -0700

    Remove old averages + update bash script

--- push_log_and_averages.sh
+++ push_log_and_averages.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 DATE=`date '+%Y%m%d'`
+python generate_averages.py
+git add averages_evening.txt
+git add averages_morning.txt
 git add log_evening.csv
 git add log_morning.csv
-git commit -m "Commute data for ${DATE}"
+git commit -m "Commute data and updated averages for ${DATE}"
 git push
